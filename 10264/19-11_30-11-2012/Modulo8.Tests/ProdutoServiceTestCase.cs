650bfb82fa64a25b307e5f643a43e54ce04e1ac8
Tiago Soczek <tiagosoczek@gmail.com>
2012-12-10 23:43:01 -0200

    Últimos dias da MOC 10264

---
+++ 10264/19-11_30-11-2012/Modulo8.Tests/ProdutoServiceTestCase.cs
@@ -0,0 +1,37 @@
+﻿namespace Modulo8.Tests
+{
+	using System.Collections.Generic;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using Model;
+	using Model.Services;
+
+	[TestClass]
+	public class ProdutoServiceTestCase
+	{
+		[TestMethod]
+		public void PesquisarProdutos_SemTermos_DeveRetornarTodos()
+		{
+			var service = new ProductService();
+
+			List<Produto> produtos = service.PesquisarProdutos(string.Empty);
+
+			Assert.IsTrue(produtos.Count > 0);
+		}
+
+		[TestMethod]
+		public void Salvar()
+		{
+			var service = new ProductService();
+
+			Produto produto = new Produto();
+
+			produto.Nome = "Falta pouco pessoal...";
+			produto.Preco = 199;
+			produto.Ativo = true;
+
+			service.Salvar(produto);
+
+			Assert.IsTrue(produto.Id > 0);
+		}
+	}
+}
---
+++ 10264/19-11_30-11-2012/Modulo8.Tests/TimespanTestCase.cs
@@ -0,0 +1,20 @@
+﻿namespace Modulo8.Tests
+{
+	using System;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class TimespanTestCase
+	{
+		[TestMethod]
+		public void SubtracaoDeDatas()
+		{
+			var agora = DateTime.Now;
+			var umAnoAtras = agora.AddYears(-1);
+
+			TimeSpan diff = (umAnoAtras - agora).Duration();
+
+			Assert.IsTrue(diff.TotalDays >= 365);
+		}
+	}
+}
