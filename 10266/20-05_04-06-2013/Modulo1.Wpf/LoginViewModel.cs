cedf5fe3bfd5dbee462430b08f1f6df9deb3e4d5
Tiago Soczek <tiagosoczek@gmail.com>
2013-05-22 00:23:45 -0300

    Adicionado projeto Modulo1.Wpf

---
+++ 10266/20-05_04-06-2013/Modulo1.Wpf/LoginViewModel.cs
@@ -0,0 +1,56 @@
+﻿namespace Modulo1.Wpf
+{
+	using System.Windows;
+	using System.Windows.Input;
+	using MVVM;
+
+	public class LoginViewModel : BaseNotifyPropertyChanged
+	{
+		private string _senha;
+		private string _usuario;
+
+		public LoginViewModel()
+		{
+			Usuario = "usuario";
+		}
+
+		public string Usuario
+		{
+			get { return _usuario; }
+			set
+			{
+				_usuario = value;
+				RaisePropertyChanged("Usuario");
+			}
+		}
+
+		public string Senha
+		{
+			get { return _senha; }
+			set
+			{
+				_senha = value;
+				RaisePropertyChanged("Senha");
+			}
+		}
+
+		public ICommand LoginCommand
+		{
+			get
+			{
+				return new RelayCommand(Logar, PodeLogar);
+			}
+		}
+
+		private bool PodeLogar()
+		{
+			return !string.IsNullOrWhiteSpace(Usuario) &&
+				!string.IsNullOrWhiteSpace(Senha);
+		}
+
+		private void Logar()
+		{
+			MessageBox.Show("Usuário: " + Usuario + ", senha: " + Senha);
+		}
+	}
+}
